0971fea55646ba9b549148f8e2638ac78cceb337
Marina Russ <mrusscode@gmail.com>
2023-06-08 15:14:31 -0400

    now Morgan only runs during dev environment

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // === GLOBAL MIDDLEWARE ===
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+};
+
 app.use(express.json());
 
 app.use((req, res, next) => {
