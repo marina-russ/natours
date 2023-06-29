a827ab8099a818dece95289c2b4b0244ef3688dc
Marina Russ <mrusscode@gmail.com>
2023-06-29 15:51:03 -0400

    corrects order of config.env and adds comment to prevent another resorting of all [const] variables first

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = require("./app");
-
 dotenv.config({ path: "./config.env" });
+//environmental variables must always be read before ./app
+const app = require("./app");
 
 const dB = process.env.DATABASE.replace(
   '<PASSWORD>',
