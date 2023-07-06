dbe52fe568d5c0541b9202529932e291ef589d82
Marina Russ <mrusscode@gmail.com>
2023-07-06 14:20:14 -0400

    adds the following features to API: filtering, sorting, limiting fields, pagination, and an alias route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,58 @@
 const Tour = require("./../models/tourModel");
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
 
+    // * BUILD QUERY
+
+    // * 1) Advanced Filtering - Using gte, gt, lte, lt
+    let queryStr = JSON.stringify(req.query);
+    // Below is regex to replace gte, gt, lte, lt with $gte, $gt, $lte, $lt for MongoDB
+    // .replace() accepts a callback which returns the new, updated string for replacing
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // * 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    };
+
+    // * 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      // __v is an internal field used by MongoDB, minus sign excludes it
+      query = query.select("-__v");
+    };
+
+    // * 4) Pagination
+    const pageResults = req.query.page * 1 || 1;
+    const limitResults = req.query.limit * 1 || 50;
+    const skipResults = (pageResults - 1) * limitResults;
+
+    query = query.skip(skipResults).limit(limitResults);
+
+    if (req.query.page) {
+      const qtyTours = await Tour.countDocuments();
+      if (skipResults >= qtyTours) throw new Error("This page does not exist");
+    };
+
+    // * EXECUTE QUERY
+    const tours = await query;
+
+    // * SEND RESPONSE
     res.status(200)
       .json({
         status: "success!",
