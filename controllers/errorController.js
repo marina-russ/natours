ed729a88fd752554b94de31f49389cb0174d595a
Marina Russ <mrusscode@gmail.com>
2023-07-28 15:31:03 -0400

    cleans code bbased on ESLint

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,9 +13,9 @@ const handleCastErrorDB = (err) => {
 // };
 
 const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map(el => el.message);
+  const errors = Object.values(err.errors).map((el) => el.message);
 
-  const message = `Invalid input data. ${errors.join('. ')}`;
+  const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
