80449263a1bc8b6dc5b64a1cad5b758e36b8a954
Marina Russ <mrusscode@gmail.com>
2023-07-28 13:10:39 -0400

    adds ability to only send messages to client for operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,49 @@
-module.exports = (err, req, res, next) => {
+const AppError = require("../utils/appError");
 
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational errors: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or unknown errors: don't leak error details
+  } else {
+    // 1) Log error to console log
+    console.error(" 💥 ERROR 💥", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };
