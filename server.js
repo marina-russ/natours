b081857f5773e0bf772d62a7302521ce9a3766dc
Marina Russ <mrusscode@gmail.com>
2023-07-28 15:33:50 -0400

    adds error handlers for uncaught rejections and exceptions

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log("UNHANDLED EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 //environmental variables must always be read before ./app
 const app = require("./app");
@@ -10,12 +16,19 @@ const dB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-//console.log(`-------------\nNODE_ENV Mode: ⚒️  ${process.env.NODE_ENV}`);
+console.log(`-------------\nNODE_ENV Mode: ⚒️  ${process.env.NODE_ENV}`);
 
-mongoose.connect(dB)
-  .then(() => console.log("DB connection successful!"));
+mongoose.connect(dB).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}!`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
