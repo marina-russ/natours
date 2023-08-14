34e74fe3f4e3dd979217d0cd7ee11050ea90b4ff
Marina Russ <mrusscode@gmail.com>
2023-08-14 13:43:50 -0400

    adds IP rate limiting to API requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -13,8 +14,14 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP. Please try again in an hour.",
+});
+app.use("/api", limiter);
 
+app.use(express.json());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
