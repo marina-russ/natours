6891be9a1c972eac1dde03fd5bf22d2a5fd68f09
Marina Russ <mrusscode@gmail.com>
2023-08-14 14:53:32 -0400

    formatting change from ESLint

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,9 @@ const signToken = (id) =>
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     // 24 hours, 60 minutes, 60 seconds, 1000 milliseconds
     httpOnly: true,
   };
