6785db1d44b63375f2f54ac1ec22bca81756e625
Marina Russ <mrusscode@gmail.com>
2023-08-17 16:02:45 -0400

    populates Tour with Review data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTourById = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate("reviews");
   // .findById is Mongoose shorthand for: Tour.findOne({ _id: req.params.id });
 
   res.status(200).json({
@@ -41,7 +41,6 @@ exports.getTourById = catchAsync(async (req, res, next) => {
 });
 
 exports.createTour = catchAsync(async (req, res, next) => {
-  // TODO: check that user is not submitting any malicious code
   const newTour = await Tour.create(req.body);
 
   res.status(201).json({
