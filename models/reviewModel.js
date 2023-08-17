d26134b2bb92d1028a1a1f14733a5c900b54b358
Marina Russ <mrusscode@gmail.com>
2023-08-17 16:01:16 -0400

    implements review model, routes, and controllers

---
+++ models/reviewModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    reviewText: {
+      type: String,
+      trim: true,
+      required: [true, "Review cannot be empty"],
+    },
+    rating: {
+      type: Number,
+      min: [1, "Rating must be 1.0 or higher"],
+      max: [5, "Rating must be 5.0 or lower"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { objects: true },
+    strictQuery: true,
+    // filter properties not in my schema (such as page, filter, sort) will not be searched
+  }
+);
+
+// Query Middleware
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "name photo",
+  });
+  next();
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
---
+++ routes/reviewRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const reviewController = require("../controllers/reviewController");
+const authController = require("../controllers/authController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createReview
+  );
+
+//router.route("/:id").get(reviewController.getReviewByID);
+// .patch(reviewController.updateReview)
+// .delete(
+//   authController.protect,
+//   authController.restrictTo("admin"),
+//   reviewController.deleteReview
+// );
+
+module.exports = router;
