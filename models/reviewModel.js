660149c276598193a15e2307f5550fd8a6443871
Marina Russ <mrusscode@gmail.com>
2023-08-24 15:19:03 -0400

    adds ability to calculate avgRating and nRating for a given tour ID

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -46,6 +47,44 @@ reviewSchema.pre(/^find/, function (next) {
     select: "name photo",
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const reviewStats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  console.log(reviewStats);
+
+  if (reviewStats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: reviewStats[0].nRating,
+      ratingsAverage: reviewStats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+// Statistics - Part 1/3 for factoring in updated or deleted reviews
+// .constructor allows us to point to the current model
+reviewSchema.post("save", function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.post(/^findOneAnd/, async (docs) => {
+  await docs.constructor.calcAverageRatings(docs.tour);
 });
 
 // =======================
