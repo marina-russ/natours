5a316313cb30efda726f9d9ace77d3ce0b8bf323
Marina Russ <mrusscode@gmail.com>
2023-08-25 17:27:23 -0400

    creates viewController file

---
+++ controllers/viewController.js
@@ -0,0 +1,15 @@
+// =======================
+// === VIEW REQUESTS
+// =======================
+
+exports.getOverview = (req, res) => {
+  res.status(200).render("overview", {
+    title: "All Tours",
+  });
+};
+
+exports.getTour = (req, res) => {
+  res.status(200).render("tour", {
+    title: "Placeholder",
+  });
+};
