409e7a9876398d4764a9be98fc3652b292766bb3
Marina Russ <mrusscode@gmail.com>
2023-08-29 17:06:37 -0400

    adds .getOverview and .getTour functionality with page rendering and dynamic data

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,15 +1,29 @@
+const Tour = require("../models/tourModel");
+// const Review = require("../models/reviewModel");
+// const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
 // =======================
 // === VIEW REQUESTS
 // =======================
 
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
+
   res.status(200).render("overview", {
     title: "All Tours",
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render("tour", {
-    title: "Placeholder",
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "reviewText rating user",
   });
-};
+
+  res.status(200).render("tour", {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
