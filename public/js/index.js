8398a391e56245163a34982dec1bb8c456f5cb21
Marina Russ <mrusscode@gmail.com>
2023-09-05 13:29:15 -0400

    creates index.js versus using a module bundler, adds leaflet.js code to the file plus login code

---
+++ public/js/index.js
@@ -0,0 +1,100 @@
+/* eslint-disable */
+
+const axios = require("axios");
+
+// =======================
+// === LOGIN
+// =======================
+const loginForm = document.querySelector(".form");
+
+// LOGIN EVENT LISTENER
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  login(email, password);
+});
+
+// LOGIN FUNCTION
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "http://127.0.0.1:3000/api/v1/users/login",
+      data: {
+        email,
+        password
+      }
+    });
+
+    if (res.data.status === "success") {
+      alert("Logged in successfully!");
+      window.setTimeout(() => {
+        location.assign("/");
+      }, 1500);
+    }
+  } catch (err) {
+    alert(err.response.data.message);
+  }
+};
+
+// =======================
+// === LEAFLET MAP
+// =======================
+
+const leafletMap = document.getElementById("map");
+
+// LEAFLET FUNCTION
+const displayMap = (locations) => {
+  // Creates map and attaches it to #map. L is the variable for using leaflet
+  const map = L.map('map', { zoomControl: false });
+
+  // Add tile layer to map
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Create icon
+  var greenIcon = L.icon({
+    iconUrl: '/img/pin.png',
+    iconSize: [32, 40],
+    iconAnchor: [16, 45], // icon point which will correspond to marker's location
+    popupAnchor: [0, -50], // point where popup opens relative to iconAnchor
+  });
+
+  // Add locations to the map
+  const points = [];
+  locations.forEach(loc => {
+    // Create points
+    points.push([loc.coordinates[1], loc.coordinates[0]]);
+
+    // Add markers
+    L.marker([loc.coordinates[1], loc.coordinates[0]], { icon: greenIcon })
+      .addTo(map)
+      // Add popup
+      .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+        autoClose: false,
+        className: 'mapPopup',
+      })
+      .on('mouseover', function (e) {
+        this.openPopup();
+      })
+      .on('mouseout', function (e) {
+        this.closePopup();
+      });
+  });
+
+  // Set map bounds to include current location
+  const bounds = L.latLngBounds(points).pad(0.5);
+  map.fitBounds(bounds);
+
+  // Disable scroll on map
+  map.scrollWheelZoom.disable();
+};
+
+// Only runs when there is a .map selector
+if (leafletMap) {
+  const locations = JSON.parse(leafletMap.dataset.locations);
+  displayMap(locations);
+};
