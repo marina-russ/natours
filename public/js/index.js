d0417bec4db8c4468e7d784b2a0855a252e97adb
Marina Russ <mrusscode@gmail.com>
2023-09-22 14:15:15 -0400

    renames updateMyData to updateMySettings, adds code to change password

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 
 import { displayMap } from "./leaflet.js";
 import { login, logout } from "./login.js";
-import { updateMyData } from "./user.js";
+import { updateMySettings } from "./user.js";
 
 // *** DOM ELEMENTS
 // *** =======================
@@ -10,6 +10,7 @@ const leafletMap = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
 const logoutButton = document.querySelector(".nav__el--logout");
 const userDataForm = document.querySelector(".form-user-data");
+const userPasswordForm = document.querySelector(".form-user-password");
 
 // *** LEAFLET MAP
 // *** =======================
@@ -42,5 +43,22 @@ if (userDataForm)
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    updateMyData(userName, email);
+    updateMySettings({ userName, email }, "data");
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    document.querySelector(".btn--save-password").textContent = "Updating...";
+
+    const passwordCurrent = document.getElementById("password-current").value;
+    const passwordNew = document.getElementById("password-new").value;
+    const passwordNewConfirm = document.getElementById("password-new-confirm").value;
+
+    await updateMySettings({ passwordCurrent, passwordNew, passwordNewConfirm }, "password");
+    document.querySelector(".btn--save-password").textContent = "Save Password";
+    // Clears the values typed into the form fields after successful API call:
+    document.getElementById("password-current").value = "";
+    document.getElementById("password-new").value = "";
+    document.getElementById("password-new-confirm").value = "";
   });
--- public/js/user.js
+++ public/js/user.js
@@ -1,23 +1,32 @@
 /* eslint-disable */
 
 import axios from 'axios';
+import { showAlert } from './alert.js';
 
-export const updateMyData = async (name, email) => {
+export const updateMySettings = async (userData, type) => {
   try {
+    const urlPass = "http://127.0.0.1:3000/api/v1/users/updateMyPassword";
+    const urlData = "http://127.0.0.1:3000/api/v1/users/updateMe";
+    const url =
+      type === "password" ? urlPass : urlData;
+
     const res = await axios({
       method: "PATCH",
-      url: "http://127.0.0.1:3000/api/v1/users/updateMe",
+      url,
+      headers: {
+        'Authorization': '',
+        'Content-Type': '',
+      },
       data: {
-        name,
-        email,
+        data: userData,
       },
     });
 
     if (res.data.status === "success") {
-      showAlert("success", "Data updated successfully!");
+      showAlert("success", `${type.toUppercase()} updated successfully!`);
     }
-
   } catch (err) {
+    console.log("Axios Error");
     showAlert("error", err.response.data.message);
   }
 };
