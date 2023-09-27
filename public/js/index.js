a04cc5d7897a28d0551b957f0f702aa9d1708561
Marina Russ <mrusscode@gmail.com>
2023-09-27 13:14:33 -0400

    adds multi-part form and html input for image upload

--- public/js/index.js
+++ public/js/index.js
@@ -41,9 +41,14 @@ if (logoutButton) logoutButton.addEventListener("click", logout);
 if (userDataForm)
   userDataForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password-new").value;
-    updateMySettings({ name, email }, "userData");
+
+    const form = new FormData();
+    form.append("name", document.getElementById("name").value);
+    form.append("email", document.getElementById("email").value);
+    form.append("photo", document.getElementById("photo").files[0]);
+    console.log("FORM 📄");
+    console.log(form);
+    updateMySettings(form, "data");
   });
 
 if (userPasswordForm)
