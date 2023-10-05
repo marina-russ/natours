d02c765aeb0604f0844b5927130d002272af3314
Marina Russ <mrusscode@gmail.com>
2023-10-05 12:23:52 -0400

    corrects syntax of imports for consistency

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
-import axios from 'axios';
-import { showAlert } from './alert.js';
+import axios from "axios";
+import { showAlert } from "./alert";
 
 // LOGIN FUNCTION
 export const login = async (email, password) => {
