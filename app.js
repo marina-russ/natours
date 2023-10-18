6ac150403d32efc3f6321d091a866976cc497dc6
Marina Russ <mrusscode@gmail.com>
2023-10-18 11:31:15 -0400

    installs compression package

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -121,6 +122,9 @@ app.use(
     ],
   })
 );
+
+// Compress text responses sent to client
+app.use(compression());
 
 // Development middleware to timestamp testing
 app.use((req, res, next) => {
