48fce4caa5281a82a55ea4fc47b5c550e85ef117
Marina Russ <mrusscode@gmail.com>
2023-10-26 15:51:50 -0400

    creates configuration file for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: "development",
-  entry: "./src/js/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
+    clean: true,
   },
+  mode: "development",
   plugins: [
     new BundleAnalyzerPlugin()
-  ]
+  ],
 };
